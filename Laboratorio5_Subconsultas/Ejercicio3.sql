a2c19da90652ed0a2ebb49d34c90172add8ba1d7
Retrogamer3000 <84058054+Retrogamer3000@users.noreply.github.com>
2025-03-16 23:45:54 -0700

    Create Ejercicio3.sql
    Puntos faltantes del ejercicio 3 (prueba)

---
+++ Laboratorio5_Subconsultas/Ejercicio3.sql
@@ -0,0 +1,4 @@
+-- 4. Listar los estados junto con la cantidad de municipios que tienen:
+--Listar: id_estado,nombre cantidad_municipios.
+--Util para estudios geograficos y administratios sobre la cantidad de municipios por estado
+SELECT
