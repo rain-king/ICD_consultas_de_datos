ff7650915d0dda8e4311f3c1a89c3bf7fa4d86e6
Gustavo Murillo Vega <43526958+rain-king@users.noreply.github.com>
2025-03-30 09:47:09 -0700

    commit inicial laboratorio 7

---
+++ Laboratorio7_ConsultasJoins/Practica4_consultasjoins_NombreAlumno.sql
@@ -0,0 +1,25 @@
+-- Equipo Whisky
+-- Alumnos: Gustavo Murillo Vega, Jose Alfredo Sanchez Grijalva, …
+-- Instructor: Gerardo Gálvez Gámez
+-- Lic. en Ing. en Ciencias de Datos
+-- Tema: Laboratorio 7 Consultas Joins
+-- Materia: Lenguajes de Consulta de Datos
+-- Fecha: 30 - Marzo - 2025
+
+-- 1. Listar las facturas registradas ordenadas por Apellido paterno del cliente.
+-- 2. Listar las facturas registradas ordenadas por Apellido paterno del cliente, con estatus cancelado.
+-- 3. Determinar el total general de facturas canceladas a un cliente específico, mostrado los campos nombre y
+-- apellidos del cliente concatenados y el total, usar alias.
+-- 4. Determinar el total general de facturas activas de un cliente específico, mostrado los campos nombre y
+-- apellidos del cliente concatenados y el total, usar alias.
+-- 5. Determinar cuántos clientes tienen facturas menores a 1000.
+-- 6. Listar el total que se le ha facturado a cada cliente.
+-------------------------------------------------------------------------------------
+
+-- 7. Listar los datos de clientes con un promedio de facturación menor a 5000.
+-- 8. Listar los datos de los clientes registrados incluyendo a los que no tienen facturas registradas, mostrar el campo
+-- nombre y apellidos del cliente y el folio y total de facturación.
+-- 9. Basado en el inciso anterior, pero mostrado los clientes que no tienen facturas registradas.
+-- 10. Obtener una unión de clientes y facturas.
+-- 11. Listar el nombre de los clientes que adquirido cierto producto.
+-- 12. Listar que productos no se encuentran en al menos una factura.
